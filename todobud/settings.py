2b731bf6b69326eab24086948c33ebc0a33a2979
Elvis Kemboi <iamelviskemboi@gmail.com>
2020-05-12 21:41:28 +0300

    Pushed some refs to 'https://git.heroku.com/todo-bud.git'

--- todobud/settings.py
+++ todobud/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 # Simplified static file serving.
