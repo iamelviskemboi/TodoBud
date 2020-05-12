d13aa61b2c819f002f82ec13bcf1e7644a3250f9
Elvis Kemboi <iamelviskemboi@gmail.com>
2020-05-12 21:50:55 +0300

    Removed the STATIC_ROOT setting from STATICFILES_DIRS

--- todobud/settings.py
+++ todobud/settings.py
@@ -123,7 +123,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Simplified static file serving.
